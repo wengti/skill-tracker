5c74121acd986448bf56546e544619015c184ba5
Weng Ti <wengti@hotmail.com>
2026-04-20 09:24:04 +0800

    put auth logo as part of auth layout

--- app/(auth)/auth/layout.tsx
+++ app/(auth)/auth/layout.tsx
@@ -1,5 +1,6 @@
 import Footer from "@/components/footers/Footer";
 import AuthHeader from "@/components/headers/AuthHeader";
+import AuthLogo from "@/components/logos/AuthLogo";
 import React from "react";
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
@@ -8,7 +9,12 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
         <>
             <AuthHeader />
             <main className='flex flex-col min-h-(--content-min-y) px-(--content-space-x) py-(--content-space-y)'>
-                {children}
+                <div className="grow flex flex-col w-full items-center justify-center">
+                    <div className="w-full max-w-sm flex flex-col gap-4">
+                        <AuthLogo />
+                        {children}
+                    </div>
+                </div>
             </main>
             <Footer />
         </>
--- app/(auth)/auth/login/page.tsx
+++ app/(auth)/auth/login/page.tsx
@@ -4,11 +4,7 @@ import { LoginForm } from "@/components/supabase-components/login-form";
 export default function Page() {
     /*  */
     return (
-        <div className="grow flex flex-col w-full items-center justify-center">
-            <div className="w-full max-w-sm flex flex-col gap-4">
-                <AuthLogo />
-                <LoginForm />
-            </div>
-        </div>
+
+        <LoginForm />
     );
 }
